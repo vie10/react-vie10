e52eef3ab00edc9cbf458614ad78ebe99ac937c1
Viktor Hrekh <hrekhviktor@gmail.com>
2022-06-15 16:27:49 +0300

    Update logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "./dev";
 import {App} from "./app/App";
@@ -13,14 +12,8 @@ root.render(
   <React.StrictMode>
     <DevSupport
       ComponentPreviews={ComponentPreviews}
-      useInitialHook={useInitial}
-    >
+      useInitialHook={useInitial}>
       <App/>
     </DevSupport>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
